b62ae93461e16bea1e8694aa15919b5d8513ea60
Akspower <adityakumarsharmapower@gmail.com>
2024-09-20 15:24:45 +0530

    refactored code

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
+
 
 connectDB();
 
